ead4174089c223a6f4f63ee1002bb99cdaf23c63
ranaharoon3222 <ranaharoon3222@gmail.com>
2023-04-11 20:01:11 +0500

    added route

---
+++ app/routes/chimp.jsx
@@ -0,0 +1,21 @@
+export const action = async ({request}) => {
+  try {
+    let data = await request.json();
+    const res = await fetch(
+      'https://us11.api.mailchimp.com/3.0/lists/19e84be598/members',
+      {
+        method: 'POST',
+        headers: {
+          Authorization: 'apikey 20793bb60c47a1ea351ecfc454c5f6a6-us11',
+        },
+        body: JSON.stringify(data),
+      },
+    );
+
+    const maichimp = await res.json();
+    console.log(maichimp);
+    return maichimp;
+  } catch (error) {
+    console.error(error);
+  }
+};
